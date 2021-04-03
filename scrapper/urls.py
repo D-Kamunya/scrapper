06ce90fa992a78cb3c0eb965641b1d17aa83deaf
Dennis Kamunya <kinc.developer@gmail.com>
2021-04-03 21:32:44 +0300

    Install drf_yasg and add configs

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        #  add your swagger doc title
+        title="INDEED JOBS API",
+        #  version of the swagger doc
+        default_version='v1',
+        # first line that appears on the top of the doc
+        description="Indeed jobs api",
+    ),
+    public=True,
+)
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
 ]
