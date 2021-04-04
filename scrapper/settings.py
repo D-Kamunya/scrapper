8ed2f28086ebe0ea9a7a6637ac1c31eec7e13d58
Dennis Kamunya <kinc.developer@gmail.com>
2021-04-04 10:08:53 +0300

    Ignore dump.rdb

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -60,9 +60,6 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
